03600cb24b7b2a364b93097c39264d6c1a5e72d9
David <itsdavid71@gmail.com>
2019-02-13 19:33:47 +0300

    update

--- template/footer.php
+++ template/footer.php
@@ -1,4 +1,11 @@
         </div>
+        <script type="text/javascript" src="https://vk.com/js/api/openapi.js?160"></script>
+
+            <!-- VK Widget -->
+            <div id="vk_community_messages"></div>
+            <script type="text/javascript">
+            VK.Widgets.CommunityMessages("vk_community_messages", 152858892, {expanded: "1",tooltipButtonText: "Есть вопрос?"});
+        </script>
         <script src="lib/jquery.pjax.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
